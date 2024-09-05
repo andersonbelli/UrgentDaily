b482aa579128556ab03e68c5629dbdc1b23113e2
Belli <ansersonbelli@gmail.com>
2024-09-05 16:31:00 -0300

    Adding 'Portuguese' localization

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -35,7 +35,8 @@ class MyApp extends StatelessWidget {
             GlobalCupertinoLocalizations.delegate,
           ],
           supportedLocales: const [
-            Locale('en', ''), // English, no country code
+            Locale('en', ''),
+            Locale('pt', ''),
           ],
           onGenerateTitle: (BuildContext context) =>
               AppLocalizations.of(context)!.appTitle,
