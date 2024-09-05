68bff752d5d50ba884501433f4fe9d4fec5ced9d
Belli <ansersonbelli@gmail.com>
2024-09-05 15:48:44 -0300

    Revert "Update text_sizes.constants.dart"
    This reverts commit f5c1cc02f3b7c61d7606fa9342e268a2d9467ff4.

--- lib/src/helpers/constants/text_sizes.constants.dart
+++ lib/src/helpers/constants/text_sizes.constants.dart
@@ -1,9 +1,5 @@
 // ignore_for_file: constant_identifier_names
 
 abstract class AppTextSize {
-  static const double textScaleFactor = 4.0;
-
-  static const double MEDIUM = 4 * textScaleFactor;
-  static const double LARGE = 6 * textScaleFactor;
-  static const double EXTRA_LARGE = 8 * textScaleFactor;
+  static const double MEDIUM = 16;
 }
