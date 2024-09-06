47c3a2d04e7db7244fd09b67069b3bad93a9170d
Belli <ansersonbelli@gmail.com>
2024-09-06 00:20:45 -0300

    Adding new colors and 'Small' TextSize

--- lib/src/helpers/constants/colors.constants.dart
+++ lib/src/helpers/constants/colors.constants.dart
@@ -6,4 +6,12 @@ abstract class AppColors {
   static const GREEN = Color.fromRGBO(143, 209, 79, 1);
   static const DARK = Color.fromRGBO(26, 26, 26, 1);
   static const DARK_LIGHT = Color.fromRGBO(51, 51, 51, 1);
+  static const GRAY = Color.fromRGBO(128, 128, 128, 1.0);
+  static const BLUE = Color.fromRGBO(57, 169, 255, 1.0);
+
+  /// Priority colors
+  static const PINK = Color.fromRGBO(218, 0, 99, 1.0);
+  static const PURPLE = Color.fromRGBO(101, 44, 179, 1.0);
+  static const BLUE_LIGHT = Color.fromRGBO(45, 155, 240, 1.0);
+  static const GRAY_LIGHT = Color.fromRGBO(206, 206, 206, 1.0);
 }
--- lib/src/helpers/constants/text_sizes.constants.dart
+++ lib/src/helpers/constants/text_sizes.constants.dart
@@ -3,6 +3,7 @@
 abstract class AppTextSize {
   static const double textScaleFactor = 4.0;
 
+  static const double SMALL = 2 * textScaleFactor;
   static const double MEDIUM = 4 * textScaleFactor;
   static const double LARGE = 6 * textScaleFactor;
   static const double EXTRA_LARGE = 8 * textScaleFactor;
