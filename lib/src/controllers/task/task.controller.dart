990a963118b4259086350926e4bad14146bc2eb4
Belli <ansersonbelli@gmail.com>
2024-09-06 22:32:46 -0300

    Adding "create task" method and implementing "edit task" to TaskController

--- lib/src/controllers/task/task.controller.dart
+++ lib/src/controllers/task/task.controller.dart
@@ -2,9 +2,16 @@ import 'package:flutter/material.dart';
 
 import '../../helpers/enums/priority.enum.dart';
 import '../../helpers/enums/recursive_days.enum.dart';
+import '../../models/task.model.dart';
 import '../base_controller.dart';
+import '../home/home.controller.dart';
 
-class TaskController extends BaseController with ChangeNotifier {
+class TaskController extends BaseController {
+  TaskController(this.homeController);
+
+  final HomeController homeController;
+
+  DateTime selectedDate = DateTime.now();
   final TextEditingController title = TextEditingController();
 
   TaskPriority taskPriority = TaskPriority.IMPORTANT;
@@ -13,6 +20,15 @@ class TaskController extends BaseController with ChangeNotifier {
   Map<RecursiveDay, bool> selectedDaysOfWeek = {};
 
   /// Task state
+  void editTaskData(Task task) {
+    title.text = task.title;
+    selectedDate = task.date ?? DateTime.now();
+    isRecursive = task.isRecursive;
+    selectedDaysOfWeek = task.recursiveDays;
+    taskPriority = task.priority;
+    notifyListeners();
+  }
+
   void toggleRecursive({bool? recursive}) {
     if (recursive == null) {
       isRecursive = !isRecursive;
@@ -23,16 +39,48 @@ class TaskController extends BaseController with ChangeNotifier {
     notifyListeners();
   }
 
+  void selectDate(DateTime newDate) {
+    selectedDate = newDate;
+    notifyListeners();
+  }
+
   void selectRecursiveDay(RecursiveDay day, bool selected) {
-    selectedDaysOfWeek[day] = selected;
+    final Map<RecursiveDay, bool> newSelectedDays =
+        Map.from(selectedDaysOfWeek);
+    newSelectedDays[day] = selected;
+    selectedDaysOfWeek = newSelectedDays;
     notifyListeners();
   }
 
   void selectTaskPriority(TaskPriority? priority) {
     if (priority != null) {
       taskPriority = priority;
-      print('task ' + taskPriority.name);
     }
+    notifyListeners();
+  }
+
+  void createTask() {
+    homeController.addTask(
+      Task(
+        title: title.text,
+        date: selectedDate,
+        isRecursive: isRecursive,
+        recursiveDays: selectedDaysOfWeek,
+        priority: taskPriority,
+      ),
+    );
+    notifyListeners();
+  }
+
+  void clearTaskData() {
+    title.clear();
+    selectedDate = DateTime.now();
+    isRecursive = false;
+    taskPriority = TaskPriority.IMPORTANT;
+
+    final Map<RecursiveDay, bool> newSelectedDays = Map.from({});
+    selectedDaysOfWeek = newSelectedDays;
+
     notifyListeners();
   }
 }
--- lib/src/models/task.model.dart
+++ lib/src/models/task.model.dart
@@ -1,60 +1,62 @@
 import '../helpers/enums/priority.enum.dart';
 import '../helpers/enums/recursive_days.enum.dart';
+import '../helpers/extensions/datetime_formatter.dart';
 
 class Task {
-
   static final mockTasksList = MockTasks.tasks;
+
+  late String? id;
 
   /// Mandatory fields
   final String title;
-  final TaskPriority taskPriority;
-  final DateTime taskDate;
-
-  /// Not mandatory - default value = day of creation
+  final TaskPriority priority;
+  final DateTime? date; // Not mandatory - default value = day of creation
 
   /// Optional fields
-  final bool taskCompleted;
+  final bool isCompleted;
   final String? description;
 
   /// Recursive fields
-  final bool recursive;
-  final List<RecursiveDay> recursiveDays;
+  final bool isRecursive;
+  final Map<RecursiveDay, bool> recursiveDays;
 
   Task({
     /// Mandatory fields
     required this.title,
-    required this.taskPriority,
-    required this.taskDate,
+    required this.priority,
+    required this.date,
 
     /// Not mandatory - default value = day of creation
 
     /// Optional fields
-    this.taskCompleted = false,
+    this.isCompleted = false,
     this.description,
 
     /// Recursive fields
-    this.recursive = false,
-    this.recursiveDays = const [],
-  });
+    this.isRecursive = false,
+    this.recursiveDays = const {},
+  }) {
+    id = '${title.trim().replaceAll(' ', '')}$priority${date?.formatDate()}${DateTime.now()}';
+  }
 
   Map<String, dynamic> toJson() => {
         'title': title,
-        'taskPriority': taskPriority,
-        'taskDate': taskDate,
-        'taskCompleted': taskCompleted,
+        'taskPriority': priority,
+        'date': date,
+        'taskCompleted': isCompleted,
         'description': description,
-        'recursive': recursive,
+        'recursive': isRecursive,
         'recursiveDays': recursiveDays,
       };
 
   factory Task.fromJson(Map<String, dynamic> map) => Task(
         title: map['title'] as String,
-        taskPriority: map['taskPriority'] as TaskPriority,
-        taskDate: map['taskDate'] as DateTime,
-        taskCompleted: map['taskCompleted'] as bool,
+        priority: map['taskPriority'] as TaskPriority,
+        date: map['date'] as DateTime,
+        isCompleted: map['taskCompleted'] as bool,
         description: map['description'] as String,
-        recursive: map['recursive'] as bool,
-        recursiveDays: map['recursiveDays'] as List<RecursiveDay>,
+        isRecursive: map['recursive'] as bool,
+        recursiveDays: map['recursiveDays'] as Map<RecursiveDay, bool>,
       );
 }
 
@@ -63,20 +65,22 @@ class MockTasks {
   static final Task _task1 = Task(
     title: 'Task 1',
     description: 'Description of Task 1',
-    taskPriority: TaskPriority.IMPORTANT,
-    taskDate: DateTime(2024, 09, 03),
+    priority: TaskPriority.IMPORTANT,
+    isRecursive: true,
+    recursiveDays: {RecursiveDay.MON: true, RecursiveDay.WED: true},
+    date: DateTime(2024, 09, 03),
   );
   static final Task _task2 = Task(
     title: 'Task 2',
     description: 'Description of Task 2',
-    taskPriority: TaskPriority.URGENT,
-    taskDate: DateTime(2024, 09, 20),
+    priority: TaskPriority.URGENT,
+    date: DateTime(2024, 09, 20),
   );
   static final Task _task3 = Task(
     title: 'Task 3',
     description: 'Description of Task 3',
-    taskPriority: TaskPriority.NOT_IMPORTANT,
-    taskDate: DateTime(2024, 09, 03),
+    priority: TaskPriority.NOT_IMPORTANT,
+    date: DateTime(2024, 09, 03),
   );
 
   static final List<Task> tasks = [_task1, _task2, _task3];
