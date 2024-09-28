c789258cf1d30d54ec85ed2a8e522ed513dabaf7
Anderson Belli <ansersonbelli@gmail.com>
2024-09-28 13:55:36 -0300

    Adding new widgets to Views

--- lib/src/views/calendar/calendar.view.dart
+++ lib/src/views/calendar/calendar.view.dart
@@ -7,6 +7,8 @@ import '../../helpers/config/di.dart';
 import '../../helpers/constants/colors.constants.dart';
 import '../../helpers/extensions/datetime_formatter.dart';
 import '../widgets/default_appbar_child.widget.dart';
+import '../widgets/loading.widget.dart';
+import 'widgets/calendar_task_item.widget.dart';
 
 class CalendarView extends StatelessWidget {
   CalendarView({super.key});
@@ -28,65 +30,72 @@ class CalendarView extends StatelessWidget {
       body: ListenableBuilder(
         listenable: calendarController,
         builder: (context, _) {
-          return Column(
+          return Stack(
             children: [
-              TableCalendar(
-                locale: AppLocalizations.of(context)!.localeName,
-                firstDay: DateTime.utc(2010, 10, 16),
-                lastDay: DateTime.utc(2030, 3, 14),
-                currentDay: calendarController.focusedDate,
-                focusedDay: calendarController.focusedDate,
-                onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
-                  calendarController.updateFocusedDate(selectedDay);
-                },
-                calendarFormat: CalendarFormat.twoWeeks,
-                headerStyle: const HeaderStyle(formatButtonVisible: false),
-                onPageChanged: (_) => calendarController.tasksAlreadyLoaded = false,
-                calendarStyle: const CalendarStyle(
-                  markerDecoration: BoxDecoration(
-                    color: AppColors.GREEN,
-                    shape: BoxShape.circle,
+              Column(
+                children: [
+                  TableCalendar(
+                    locale: AppLocalizations.of(context)!.localeName,
+                    firstDay: DateTime.utc(2010, 10, 16),
+                    lastDay: DateTime.utc(2030, 3, 14),
+                    currentDay: calendarController.focusedDate,
+                    focusedDay: calendarController.focusedDate,
+                    onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
+                      calendarController.updateFocusedDate(selectedDay);
+                    },
+                    calendarFormat: CalendarFormat.twoWeeks,
+                    headerStyle: const HeaderStyle(formatButtonVisible: false),
+                    onPageChanged: (_) => calendarController.tasksAlreadyLoaded = false,
+                    calendarStyle: const CalendarStyle(
+                      markerDecoration: BoxDecoration(
+                        color: AppColors.GREEN,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                    daysOfWeekStyle: DaysOfWeekStyle(
+                      weekdayStyle: TextStyle(
+                        color: Theme.of(context).indicatorColor,
+                      ),
+                      weekendStyle: TextStyle(
+                        color: Theme.of(context).indicatorColor.withOpacity(0.8),
+                      ),
+                    ),
+                    eventLoader: (DateTime date) {
+                      calendarController.updateVisibleDates(date.formatDate());
+
+                      final events = [];
+
+                      for (final task in calendarController.twoWeeksTasks) {
+                        if (task.date.formatDate() == date.formatDate()) {
+                          events.addAll(task.tasks);
+                        }
+                      }
+
+                      return events;
+                    },
                   ),
-                ),
-                daysOfWeekStyle: DaysOfWeekStyle(
-                  weekdayStyle: TextStyle(
-                    color: Theme.of(context).indicatorColor,
+                  DefaultAppBarChild(
+                    Text(
+                      calendarController.focusedDate.formatDate(),
+                    ),
                   ),
-                  weekendStyle: TextStyle(
-                    color: Theme.of(context).indicatorColor.withOpacity(0.8),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: calendarController.tasksOfSelectedDay.length,
+                      itemBuilder: (context, index) {
+                        final task = calendarController.tasksOfSelectedDay[index];
+
+                        return CalendarTaskItem(
+                          title: task.title,
+                          priority: task.priority,
+                          isCompleted: task.isCompleted,
+                        );
+                      },
+                    ),
                   ),
-                ),
-                eventLoader: (DateTime date) {
-                  calendarController.updateVisibleDates(date.formatDate());
-
-                  final events = [];
-
-                  for (final task in calendarController.twoWeeksTasks) {
-                    if (task.date.formatDate() == date.formatDate()) {
-                      events.addAll(task.tasks);
-                    }
-                  }
-
-                  return events;
-                },
+                ],
               ),
-              DefaultAppBarChild(
-                Text(
-                  calendarController.focusedDate.formatDate(),
-                ),
-              ),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: calendarController.tasksOfSelectedDay.length,
-                  itemBuilder: (context, index) {
-                    final task = calendarController.tasksOfSelectedDay[index];
-
-                    return Text(
-                      task.title,
-                    );
-                  },
-                ),
-              ),
+              loadingWidget(calendarController.isLoading),
             ],
           );
         },
--- lib/src/views/home/home.view.dart
+++ lib/src/views/home/home.view.dart
@@ -5,15 +5,15 @@ import '../../controllers/home/home.controller.dart';
 import '../../controllers/task/task.controller.dart';
 import '../../helpers/config/di.dart';
 import '../../helpers/constants/colors.constants.dart';
-import '../../helpers/constants/padding.constants.dart';
+import '../../helpers/enums/priority.enum.dart';
 import '../../helpers/extensions/datetime_formatter.dart';
-import '../../models/task.model.dart';
 import '../calendar/calendar.view.dart';
-import '../task/task.view.dart';
 import '../widgets/default_appbar_child.widget.dart';
+import '../widgets/loading.widget.dart';
 import '../widgets/text_underline.widget.dart';
 import 'widgets/create_new_task.widget.dart';
 import 'widgets/no_tasks_yet.widget.dart';
+import 'widgets/task_section.widget.dart';
 
 class HomeView extends StatelessWidget {
   HomeView({
@@ -69,7 +69,7 @@ class HomeView extends StatelessWidget {
                           listOfSections.add(
                             TaskSection(
                               title: AppLocalizations.of(context)!.urgent,
-                              color: AppColors.PINK.withOpacity(0.5),
+                              color: TaskPriority.URGENT.color.withOpacity(0.5),
                               tasks: homeController.urgentTasks,
                             ),
                           );
@@ -79,7 +79,7 @@ class HomeView extends StatelessWidget {
                           listOfSections.add(
                             TaskSection(
                               title: AppLocalizations.of(context)!.important,
-                              color: AppColors.PURPLE.withOpacity(0.5),
+                              color: TaskPriority.IMPORTANT.color.withOpacity(0.5),
                               tasks: homeController.importantTasks,
                             ),
                           );
@@ -88,9 +88,8 @@ class HomeView extends StatelessWidget {
                         if (homeController.importantNotUrgentTasks.isNotEmpty) {
                           listOfSections.add(
                             TaskSection(
-                              title: AppLocalizations.of(context)!
-                                  .importantNotUrgent,
-                              color: AppColors.BLUE_LIGHT.withOpacity(0.5),
+                              title: AppLocalizations.of(context)!.importantNotUrgent,
+                              color: TaskPriority.IMPORTANT_NOT_URGENT.color.withOpacity(0.5),
                               tasks: homeController.importantNotUrgentTasks,
                             ),
                           );
@@ -100,7 +99,7 @@ class HomeView extends StatelessWidget {
                           listOfSections.add(
                             TaskSection(
                               title: AppLocalizations.of(context)!.notImportant,
-                              color: AppColors.GRAY.withOpacity(0.3),
+                              color: TaskPriority.NOT_IMPORTANT.color.withOpacity(0.3),
                               tasks: homeController.notImportantTasks,
                             ),
                           );
@@ -122,170 +121,12 @@ class HomeView extends StatelessWidget {
                         );
                       },
                     ),
-              _loadingWidget,
+              loadingWidget(homeController.isLoading),
             ],
           ),
         );
       },
       child: const NoTasksYet(),
-    );
-  }
-
-  Widget get _loadingWidget => homeController.isLoading
-      ? Container(
-          color: AppColors.DARK.withOpacity(0.8),
-          width: double.infinity,
-          height: double.infinity,
-          child: const Center(
-            child: CircularProgressIndicator(
-              valueColor: AlwaysStoppedAnimation(AppColors.GREEN),
-            ),
-          ),
-        )
-      : const SizedBox.shrink();
-}
-
-class TaskItem extends StatelessWidget {
-  const TaskItem({
-    super.key,
-    required this.task,
-    required this.homeController,
-    required this.taskController,
-  });
-
-  final Task task;
-  final HomeController homeController;
-  final TaskController taskController;
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(
-        task.title,
-        style: TextStyle(
-          decoration: task.isCompleted
-              ? TextDecoration.lineThrough
-              : TextDecoration.none,
-        ),
-      ),
-      onTap: () => homeController.toggleCompletedTask(
-        task,
-        !task.isCompleted,
-      ),
-      leading: IconButton(
-        onPressed: () {
-          showModalBottomSheet(
-            context: context,
-            isScrollControlled: true,
-            builder: (context) {
-              taskController.editTaskData(
-                task,
-              );
-
-              return FractionallySizedBox(
-                heightFactor: 0.9,
-                child: TaskView(),
-              );
-            },
-          );
-        },
-        icon: const Icon(
-          Icons.edit,
-        ),
-      ),
-      trailing: Checkbox(
-        value: task.isCompleted,
-        onChanged: (isTaskCompleted) => homeController.toggleCompletedTask(
-          task,
-          isTaskCompleted,
-        ),
-        checkColor: AppColors.DARK,
-        activeColor: AppColors.GREEN,
-      ),
-    );
-  }
-}
-
-class TaskSection extends StatelessWidget {
-  const TaskSection({
-    super.key,
-    required this.title,
-    required this.color,
-    required this.tasks,
-  });
-
-  final String title;
-  final Color color;
-  final List<Task> tasks;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(
-        horizontal: AppPadding.MEDIUM,
-        vertical: AppPadding.SMALL,
-      ),
-      padding: const EdgeInsets.symmetric(horizontal: AppPadding.MEDIUM),
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(8),
-        boxShadow: [
-          BoxShadow(
-            color: AppColors.GRAY.withOpacity(0.2),
-            spreadRadius: 1,
-            blurRadius: 2,
-            offset: const Offset(0, 2),
-          ),
-        ],
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          Container(
-            padding: const EdgeInsets.symmetric(
-              vertical: AppPadding.SMALL,
-              horizontal: AppPadding.LARGE,
-            ),
-            decoration: BoxDecoration(
-              color: Colors.amber,
-              borderRadius: const BorderRadius.only(
-                bottomLeft: Radius.circular(4),
-                bottomRight: Radius.circular(4),
-              ),
-              boxShadow: [
-                BoxShadow(
-                  color: AppColors.DARK.withOpacity(0.2),
-                  spreadRadius: 2,
-                  blurRadius: 8,
-                  offset: const Offset(0, 3),
-                ),
-              ],
-            ),
-            child: Text(
-              title,
-              style: const TextStyle(
-                color: AppColors.DARK,
-                fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.italic,
-              ),
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.symmetric(
-              vertical: AppPadding.MEDIUM,
-            ),
-            child: ListView.builder(
-              shrinkWrap: true,
-              itemCount: tasks.length,
-              itemBuilder: (context, index) => TaskItem(
-                task: tasks[index],
-                homeController: getIt<HomeController>(),
-                taskController: getIt<TaskController>(),
-              ),
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
