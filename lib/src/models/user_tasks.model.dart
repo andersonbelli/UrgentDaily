372e8290aa11ef9dc9443134cfed2e07861dec10
Anderson Belli <ansersonbelli@gmail.com>
2024-10-07 19:17:07 -0300

    Implementing new methods for UserTasks

--- lib/src/models/user_tasks.model.dart
+++ lib/src/models/user_tasks.model.dart
@@ -16,6 +16,29 @@ class UserTasks {
         date: date ?? this.date,
         tasks: tasks ?? this.tasks,
       );
+
+  factory UserTasks.fromListOfTasks({
+    required List<Task> tasks,
+    required DateTime date,
+  }) =>
+      UserTasks(
+        tasks: tasks,
+        date: date,
+      );
+
+  factory UserTasks.orderTasksByPriority({
+    required List<Task> tasks,
+    required DateTime date,
+  }) {
+    final userTasks = UserTasks.fromListOfTasks(
+      tasks: List.from(tasks),
+      date: date,
+    );
+    userTasks.tasks.sort(
+      (a, b) => a.priority.index.compareTo(b.priority.index),
+    );
+    return userTasks;
+  }
 }
 
 /// Mock Tasks data
