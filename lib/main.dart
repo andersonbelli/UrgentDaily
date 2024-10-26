249ec9fe6ddd203665ab488e1f68eed6b11320c1
Belli <ansersonbelli@gmail.com>
2024-10-26 00:33:58 -0300

    24 connect app to firebase (#27)
    * Updating ReadMe; Upgrading Lints and adding Firebase_core
    
    * Creating sample.env File and updating gitignore
    
    * Adding Google dependencies for Android and DotEnv
    
    * Creating Env constants and envFlavor variable
    
    * Loading Firebase Environment depending on Flavor
    
    * Rollback project name to urgent_daily

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'firebase_options.dart';
 import 'src/app.dart';
 import 'src/controllers/settings/settings.controller.dart';
 import 'src/helpers/di/di.dart';
@@ -10,6 +12,10 @@ void main() async {
   configureDependencies();
 
   await getIt.get<SettingsController>().loadSettings();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(MyApp());
 }
