ce34486b5d005acfd67230706224d619ac22a435
Belli <ansersonbelli@gmail.com>
2024-10-26 00:23:13 -0300

    Creating Env constants and envFlavor variable

--- lib/src/services/settings/settings.service.dart
+++ lib/src/services/settings/settings.service.dart
@@ -1,6 +1,24 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../../helpers/constants/env_flavors.constants.dart';
 
 class SettingsService {
+  String get envFlavor {
+    String? flavor = 'dev';
+    if (appFlavor != null && envFlavors.containsKey(appFlavor)) {
+      flavor = appFlavor;
+    } else {
+      log('''
+      \nFlavor not defined: <$appFlavor>
+      Use the flag --flavor=<dev|stg|prod> to set the flavor.
+      ''');
+    }
+    return envFlavors[flavor]!;
+  }
+
   Future<ThemeMode> themeMode() async => ThemeMode.light;
 
   Future<void> updateThemeMode(ThemeMode theme) async {
