2f2ebd7521ef45a15dd7af2bff3137ad58d7c62b
Belli <ansersonbelli@gmail.com>
2024-11-27 23:34:22 -0300

    Partial: Creating SignIn and SignUp view

---
+++ lib/src/helpers/enums/error_fields/sign_in_error_fields.enum.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: constant_identifier_names
+
+import '../../../localization/localization.dart';
+import 'error_fields.dart';
+
+enum SignInErrorFieldsEnum implements ErrorFields {
+  EMAIL_CANT_BE_EMPTY,
+  PASSWORD_CANT_BE_EMPTY,
+  INCORRECT_EMAIL_OR_PASSWORD;
+
+  @override
+  String get message {
+    switch (this) {
+      case EMAIL_CANT_BE_EMPTY:
+        return t.emailCantBeEmpty;
+      case PASSWORD_CANT_BE_EMPTY:
+        return t.passwordCantBeEmpty;
+      case INCORRECT_EMAIL_OR_PASSWORD:
+        return t.incorrectEmailOrPassword;
+      default:
+        return '';
+    }
+  }
+}
---
+++ lib/src/views/auth/sign_up/sign_up.view.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+import '../../../controllers/auth/sign_up.controller.dart';
+import '../../../helpers/constants/colors.constants.dart';
+import '../../../helpers/constants/padding.constants.dart';
+import '../../../helpers/di/di.dart';
+import '../../../localization/localization.dart';
+import '../../widgets/error_messages_container.widget.dart';
+import '../../widgets/text_shadow.widget.dart';
+
+class SignUpView extends StatefulWidget {
+  const SignUpView({super.key});
+
+  @override
+  State<SignUpView> createState() => _SignUpViewState();
+}
+
+class _SignUpViewState extends State<SignUpView> {
+  final auth = getIt<SignUpController>();
+
+  void _showErrorDialog(String message) => showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: Text(t.errorTitle),
+          content: Text(message),
+          actions: <Widget>[
+            TextButton(
+              child: Text(t.errorOkButton),
+              onPressed: () => Navigator.of(ctx).pop(),
+            ),
+          ],
+        ),
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ListTile(
+          dense: true,
+          trailing: IconButton(
+            icon: Icon(
+              Icons.close,
+              color: Theme.of(context).colorScheme.inverseSurface,
+              shadows: [
+                BoxShadow(
+                  color: AppColors.DARK.withOpacity(0.3),
+                  blurRadius: 0.4,
+                  offset: const Offset(0, 2),
+                ),
+              ],
+            ),
+            onPressed: () => Navigator.pop(context),
+          ),
+          title: TextShadow(text: t.signUp),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(AppPadding.MEDIUM),
+          child: Form(
+            key: auth.formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextFormField(
+                  controller: auth.emailController,
+                  decoration: const InputDecoration(labelText: 'Email'),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your email';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  controller: auth.passwordController,
+                  decoration: const InputDecoration(labelText: 'Password'),
+                  obscureText: true,
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  controller: auth.confirmPasswordController,
+                  decoration:
+                      const InputDecoration(labelText: 'Confirm Password'),
+                  obscureText: true,
+                  validator: (value) {
+                    if (value != auth.passwordController.text) {
+                      return 'Passwords do not match';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  child: const Text('Sign Up'),
+                  onPressed: () async {
+                    if (auth.formKey.currentState!.validate()) {
+                      try {
+                        await auth.signUpWithEmail(
+                          auth.emailController.text,
+                          auth.passwordController.text,
+                        );
+
+                        if (context.mounted) Navigator.pop(context);
+                      } catch (e) {
+                        _showErrorDialog(e.toString());
+                      }
+                    }
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+        ErrorMessagesContainer(
+          isVisible: auth.validationErrorMessages.isNotEmpty,
+          errorMessagesList: auth.validationErrorMessages,
+        ),
+      ],
+    );
+  }
+}
