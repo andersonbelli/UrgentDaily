c1e0f540ab342e1358db38f80c04e1c9d98228f2
Anderson Belli <ansersonbelli@gmail.com>
2025-04-12 12:23:03 +0100

    TaskModel: adding full date and user id fields

--- lib/src/models/task.model.dart
+++ lib/src/models/task.model.dart
@@ -2,14 +2,17 @@ import 'package:uuid/uuid.dart';
 
 import '../helpers/enums/priority.enum.dart';
 import '../helpers/enums/recursive_days.enum.dart';
+import '../helpers/extensions/datetime_formatter.dart';
 
 class Task {
   late String? id;
 
   /// Mandatory fields
+  final String userId;
   final String title;
   final TaskPriority priority;
-  final DateTime? date; // Not mandatory - default value = day of creation
+  final String? date; // Not mandatory - default value = day of creation
+  final DateTime? fullDate;
 
   /// Optional fields
   final bool isCompleted;
@@ -23,9 +26,11 @@ class Task {
     this.id,
 
     /// Mandatory fields
+    required this.userId,
     required this.title,
     required this.priority,
     required this.date,
+    this.fullDate,
 
     /// Not mandatory - default value = day of creation
 
@@ -42,36 +47,40 @@ class Task {
 
   Map<String, dynamic> toJson() => {
         'id': id,
+        'userId': userId,
         'title': title,
         'priority': priority.index,
-        'date': date?.toIso8601String(),
+        'date': date,
+        'fullDate': fullDate?.toIso8601String(),
         'isCompleted': isCompleted,
         'description': description,
         'isRecursive': isRecursive,
         'recursiveDays': recursiveDays.map((key, value) => MapEntry(key.index.toString(), value)),
       };
 
-  factory Task.fromFirebaseMap(Map<String, dynamic> map) {
-    return Task(
-      id: map['id'],
-      title: map['title'],
-      priority: TaskPriority.values[map['priority']], // Convert index back to Enum
-      date: map['date'] != null ? DateTime.parse(map['date']) : null,
-      isCompleted: map['isCompleted'] ?? false,
-      description: map['description'],
-      isRecursive: map['isRecursive'] ?? false,
-      recursiveDays: (map['recursiveDays'] as Map<String, dynamic>?)?.map(
-            (key, value) => MapEntry(RecursiveDay.values[int.parse(key)], value as bool),
-          ) ??
-          {},
-    );
-  }
+  factory Task.fromFirebaseMap(Map<String, dynamic> map) => Task(
+        id: map['id'],
+        userId: map['userId'],
+        title: map['title'],
+        priority: TaskPriority.values[map['priority']], // Convert index back to Enum
+        date: map['date'],
+        fullDate: map['fullDate'] != null ? DateTime.parse(map['fullDate']) : null,
+        isCompleted: map['isCompleted'] ?? false,
+        description: map['description'],
+        isRecursive: map['isRecursive'] ?? false,
+        recursiveDays: (map['recursiveDays'] as Map<String, dynamic>?)?.map(
+              (key, value) => MapEntry(RecursiveDay.values[int.parse(key)], value as bool),
+            ) ??
+            {},
+      );
 
   factory Task.fromJson(Map<String, dynamic> map) => Task(
         id: map['id'] as String,
+        userId: map['userId'] as String,
         title: map['title'] as String,
         priority: map['taskPriority'] as TaskPriority,
-        date: map['date'] as DateTime,
+        date: map['date'] as String,
+        fullDate: map['fullDate'] as DateTime,
         isCompleted: map['taskCompleted'] as bool,
         description: map['description'] as String,
         isRecursive: map['recursive'] as bool,
@@ -80,9 +89,11 @@ class Task {
 
   Task copyWith({
     String? id,
+    String? userId,
     String? title,
     TaskPriority? priority,
-    DateTime? date,
+    String? date,
+    DateTime? fullDate,
     bool? isCompleted,
     String? description,
     bool? isRecursive,
@@ -90,9 +101,11 @@ class Task {
   }) =>
       Task(
         id: id ?? this.id,
+        userId: userId ?? this.userId,
         title: title ?? this.title,
         priority: priority ?? this.priority,
         date: date ?? this.date,
+        fullDate: fullDate ?? this.fullDate,
         isCompleted: isCompleted ?? this.isCompleted,
         description: description ?? this.description,
         isRecursive: isRecursive ?? this.isRecursive,
