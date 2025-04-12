12c840ddb6b83bbf171b45a6fe4e9a6a13953160
Anderson Belli <ansersonbelli@gmail.com>
2025-04-12 11:36:08 +0100

    Delay ShowToastMessage and check if user is logged in

--- lib/src/controllers/base_controller.dart
+++ lib/src/controllers/base_controller.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/foundation.dart';
 
+import '../helpers/di/di.dart';
+import '../services/auth/auth.service.dart';
+
 class BaseController with ChangeNotifier {
   String? _message;
 
   String? get message => _message;
 
-  void showMessage(String newMessage) {
+  void showToastMessage(String newMessage) async {
     _message = newMessage;
     notifyListeners();
+    await Future.delayed(const Duration(seconds: 1));
     _message = null; // Reset after notifying
   }
 
@@ -19,6 +23,25 @@ class BaseController with ChangeNotifier {
     _isLoading = !_isLoading;
 
     notifyListeners();
+  }
+
+  // TODO: save this data on local database -> check only once per session
+  bool? _checkedIfUserIsLoggedIn;
+
+  bool? get checkedIfUserIsLoggedIn => _checkedIfUserIsLoggedIn;
+  set checkedIfUserIsLoggedIn(bool? value) {
+    _checkedIfUserIsLoggedIn = value;
+    notifyListeners();
+  }
+
+  bool get isUserLoggedIn {
+    final user = getIt.get<AuthService>().user;
+
+    if (user.isAnonymous || (user.email?.isEmpty ?? true)) {
+      return false;
+    }
+
+    return true;
   }
 
   Future<T?> apiCall<T>({
