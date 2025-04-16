183bf47363bdb571d907d78af540996221e50c69
Anderson Belli <ansersonbelli@gmail.com>
2025-04-16 01:07:14 +0100

    Calendar picker: Fixing bugs and improving UI

--- lib/src/views/task/widgets/calendar_picker.widget.dart
+++ lib/src/views/task/widgets/calendar_picker.widget.dart
@@ -1,8 +1,12 @@
+import 'package:abelliz_essentials/constants/padding.constants.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../../../controllers/settings/settings.controller.dart';
 import '../../../controllers/task/task.controller.dart';
 import '../../../helpers/constants/colors.constants.dart';
 import '../../../helpers/extensions/datetime_formatter.dart';
+import '../../../localization/localization.dart';
 
 class CalendarPickerWidget extends StatelessWidget {
   const CalendarPickerWidget({super.key, required this.taskController});
@@ -13,22 +17,65 @@ class CalendarPickerWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () async {
-        final newDateSelected = await showDatePicker(
+        final minimunDate = DateTime.utc(DateTime.now().year - 10);
+        final maximunDate = DateTime.utc(DateTime.now().year + 10);
+
+        await showDatePicker(
           context: context,
-          builder: (BuildContext context, widget) {
-            return DatePickerDialog(
-              firstDate: DateTime.now(),
-              lastDate: DateTime.utc(2030),
-              currentDate: taskController.selectedDate,
+          firstDate: minimunDate,
+          lastDate: maximunDate,
+          locale: SettingsController.locale,
+          builder: (context, _) {
+            return Stack(
+              children: [
+                Container(
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: AppPadding.kSize16,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(30),
+                    ),
+                    color: Theme.of(context).colorScheme.surface,
+                  ),
+                  child: CupertinoDatePicker(
+                    onDateTimeChanged: (newDate) => taskController.selectedDate = newDate,
+                    mode: CupertinoDatePickerMode.date,
+                    itemExtent: 120,
+                    dateOrder: DatePickerDateOrder.ymd,
+                    initialDateTime: taskController.selectedDate,
+                    minimumDate: minimunDate,
+                    maximumDate: maximunDate,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 48),
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        CupertinoButton.tinted(
+                          onPressed: () => Navigator.pop(context),
+                          color: AppColors.RED,
+                          sizeStyle: CupertinoButtonSize.medium,
+                          child: Text(
+                            t.cancel,
+                            style: TextStyle(color: Theme.of(context).colorScheme.inverseSurface),
+                          ),
+                        ),
+                        CupertinoButton.filled(
+                          onPressed: () => Navigator.pop(context),
+                          child: Text(t.okay),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             );
           },
-          firstDate: DateTime.now(),
-          lastDate: DateTime.utc(2030),
         );
-
-        if (newDateSelected != null) {
-          taskController.selectDate(newDateSelected);
-        }
       },
       child: SizedBox(
         width: double.infinity,
