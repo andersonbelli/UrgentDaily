3a4fa7df48bafb25d4fe50a312dd49e7888315f8
Anderson Belli <ansersonbelli@gmail.com>
2025-04-18 23:33:51 +0100

    UI Update: Errors Message Widget

--- lib/src/views/widgets/error_messages_container.widget.dart
+++ lib/src/views/widgets/error_messages_container.widget.dart
@@ -1,3 +1,4 @@
+import 'package:abelliz_essentials/constants/colors.constants.dart';
 import 'package:flutter/material.dart';
 
 import '../../helpers/constants/padding.constants.dart';
@@ -22,17 +23,27 @@ class ErrorMessagesContainer extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.all(AppPadding.size16),
       color: Theme.of(context).focusColor,
-      child: ListView(
-        shrinkWrap: true,
-        children: errorMessagesList.values.map((message) {
+      child: ListView.separated(
+        separatorBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.only(right: 88.0),
+            child: Divider(
+              color: AppColors.GRAY.withValues(alpha: 0.4),
+            ),
+          );
+        },
+        itemCount: errorMessagesList.values.length,
+        itemBuilder: (context, index) {
+          final errorMessage = errorMessagesList.values.elementAt(index);
+
           return Text(
-            '* $message',
+            '* $errorMessage',
             textAlign: TextAlign.start,
             style: Theme.of(context).textTheme.labelMedium?.copyWith(
                   color: Theme.of(context).colorScheme.error,
                 ),
           );
-        }).toList(),
+        },
       ),
     );
   }
